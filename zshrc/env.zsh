3b50ab14b4d42f67d1efa5ec685656f64303a58e
Logan Rose <logprose@umich.edu>
2025-12-20 20:52:17 -0500

    feat: added nvim as default for man pages

--- zshrc/env.zsh
+++ zshrc/env.zsh
@@ -1,2 +1,8 @@
 export GOBIN=~/go/bin
 export PATH=$PATH:~/go/bin
+if command -v nvim &> /dev/null
+then
+    export MANPAGER="nvim +Man!"
+else
+    export MANPAGER="vim +MANPAGER --not-a-term -"
+fi
