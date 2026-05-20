f66d7e889f87d1bc51f8364dc33ebba4f4f3a700
Logan Rose <logprose@umich.edu>
2026-05-19 22:24:02 -0400

    feat: lc alias + note for self

--- zshrc/alias.zsh
+++ zshrc/alias.zsh
@@ -5,3 +5,7 @@ alias ls="eza --color=always --long --no-filesize --icons=always --no-time --no-
 eval "$(zoxide init zsh)"
 
 alias cd="z"
+
+# can insert file type for non alias
+# e.g. git ls-files "*.rs" | xargs wc -l
+alias lc="git ls-files | xargs wc -l"
