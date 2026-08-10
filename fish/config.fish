90831ee3fb25300335f5e3c4bb15ecf82471de0f
Logan Rose <logprose@umich.edu>
2026-08-10 17:43:07 +0000

    feat: peel those prying eyes away

--- fish/config.fish
+++ fish/config.fish
@@ -9,5 +9,13 @@ starship init fish | source
 function firefox
     firefox-developer-edition > /dev/null 2>&1 &
 end
+
+function protonvpn
+    if test "$argv[1]" = "connect"; and test (count $argv) -eq 1
+        command protonvpn connect --country CH
+    else
+        command protonvpn $argv
+    end
+end
 # alias
 alias vim='nvim'
